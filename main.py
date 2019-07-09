4a419d20c77c20e921b54b1a787c8548cafbced8
RizhaoCai <37952703+RizhaoCai@users.noreply.github.com>
2019-07-09 14:06:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import sys
 sys.append('.\')
 import tensorflow as tf
 import alexnet as net
-import utils.data_utils as data
+import utils.data_prepare as data
 def train():
-    dataset_path = ""
     sess = tf.InteractiveSession()
     model = net.AlexNet()
-    stride = 100
-    X_train, y_train = data.load_data(dataset_path,"TRAIN", stride)
+    tf_record_path = './data.tf' # This should be prepared in advance
+    data_iterator = data(tf_record_path, num_epochs=2)
+    X_train_it, _, y_train_it = data_iterator.get_next()
+    X_train, _y_train = sess.run([X_train_it, y_train_it])
     model.train(X_train, y_train, sess)
     
 def test():
-    pass
+    pass # Not Implemented
 if __name__ == '__main__':
     train()
